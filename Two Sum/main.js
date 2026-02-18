e98b4e1799be6f6d2646f48bc97f44e629731fcd
glitchwhostolexmas <justin.raf.jimenez@gmail.com>
2026-02-18 11:16:33 -0500

    two more solutions

---
+++ Two Sum/main.js
@@ -0,0 +1,40 @@
+// Problem:
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+// You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+// Return the answer with the smaller index first.
+
+// Example 1:
+
+// Input: 
+// nums = [3,4,5,6], target = 7
+
+// Output: [0,1]
+
+// Solution:
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] === target) {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+}
+
